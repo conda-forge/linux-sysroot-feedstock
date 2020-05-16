a8a3326d72b66de63f1d3f38d82832783fa6b73a
Matthew R Becker <becker.mr@gmail.com>
2020-05-16 07:24:28 -0500

    try again

--- recipe/build-sysroot.sh
+++ recipe/build-sysroot.sh
@@ -10,9 +10,13 @@ cp -Rf "${SRC_DIR}"/binary-glibc-headers/include/* usr/include/
 cp -Rf "${SRC_DIR}"/binary-glibc-devel/* usr/
 cp -Rf "${SRC_DIR}"/binary-glibc-common/* .
 
-mv lib64 lib
+mkdir -p lib
+mv lib64/* lib
+rm -rf lib64
+
 mv usr/lib64/* usr/lib/
 rm -rf usr/lib64
+
 ln -s $PWD/lib $PWD/lib64
 ln -s $PWD/usr/lib $PWD/usr/lib64
 
