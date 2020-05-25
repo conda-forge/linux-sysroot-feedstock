3b30ec606e281ec53cdbd82d980400e402d55114
Matthew R Becker <becker.mr@gmail.com>
2020-05-25 10:12:55 -0500

    for hacking

--- recipe/build-sysroot.sh
+++ recipe/build-sysroot.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-mkdir -p ${PREFIX}/${target_machine}-conda-linux-gnu/sysroot
-pushd ${PREFIX}/${target_machine}-conda-linux-gnu/sysroot > /dev/null 2>&1
-cp -Rf "${SRC_DIR}"/binary/* .
-mkdir -p usr/include
-cp -Rf "${SRC_DIR}"/binary-tzdata/* usr/
-cp -Rf "${SRC_DIR}"/binary-kernel-headers/include/* usr/include/
-cp -Rf "${SRC_DIR}"/binary-glibc-headers/include/* usr/include/
-cp -Rf "${SRC_DIR}"/binary-glibc-devel/* usr/
-cp -Rf "${SRC_DIR}"/binary-glibc-common/* .
-
-mkdir -p usr/lib
-mkdir -p usr/lib64
-mv usr/lib/* usr/lib64/
-rm -rf usr/lib
-ln -s $PWD/usr/lib64 $PWD/usr/lib
-
-ln -s $PWD/lib64 $PWD/lib
-
-popd
+# mkdir -p ${PREFIX}/${target_machine}-conda-linux-gnu/sysroot
+# pushd ${PREFIX}/${target_machine}-conda-linux-gnu/sysroot > /dev/null 2>&1
+# cp -Rf "${SRC_DIR}"/binary/* .
+# mkdir -p usr/include
+# cp -Rf "${SRC_DIR}"/binary-tzdata/* usr/
+# cp -Rf "${SRC_DIR}"/binary-kernel-headers/include/* usr/include/
+# cp -Rf "${SRC_DIR}"/binary-glibc-headers/include/* usr/include/
+# cp -Rf "${SRC_DIR}"/binary-glibc-devel/* usr/
+# cp -Rf "${SRC_DIR}"/binary-glibc-common/* .
+#
+# mkdir -p usr/lib
+# mkdir -p usr/lib64
+# mv usr/lib/* usr/lib64/
+# rm -rf usr/lib
+# ln -s $PWD/usr/lib64 $PWD/usr/lib
+#
+# ln -s $PWD/lib64 $PWD/lib
+#
+# popd
