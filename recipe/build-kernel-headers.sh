e92c888e58365d0cc9a565f6c5b3ba75264f1268
Matthew R Becker <becker.mr@gmail.com>
2020-05-31 15:54:23 -0500

    BUG no files here

--- recipe/build-kernel-headers.sh
+++ recipe/build-kernel-headers.sh
@@ -2,7 +2,6 @@
 
 mkdir -p ${PREFIX}/${target_machine}-conda-linux-gnu/sysroot
 pushd ${PREFIX}/${target_machine}-conda-linux-gnu/sysroot > /dev/null 2>&1
-cp -Rf "${SRC_DIR}"/binary/* .
 mkdir -p usr/include
 cp -Rf "${SRC_DIR}"/binary-kernel-headers/include/* usr/include/
 popd
