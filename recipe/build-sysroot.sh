e4bb9b1f760ee5d531e6c1d499132f7539a27690
Matthew R Becker <becker.mr@gmail.com>
2020-06-06 07:34:58 -0500

    fix paths

--- recipe/build-sysroot.sh
+++ recipe/build-sysroot.sh
@@ -18,5 +18,15 @@ ln -s $PWD/usr/lib64 $PWD/usr/lib
 popd
 
 if [[ ${ctng_vendor} == "conda_cos6" ]]; then
-    ln -s ${PREFIX}/${target_machine}-${ctng_vendor}-linux-gnu ${PREFIX}/${target_machine}-conda-linux-gnu
+  mkdir -p ${PREFIX}/${target_machine}-conda-linux-gnu/sysroot
+  pushd ${PREFIX}/${target_machine}-${ctng_vendor}-linux-gnu/sysroot
+  for dn in $(ls -d */); do
+    mkdir -p ${PREFIX}/${target_machine}-conda-linux-gnu/sysroot/${dn}
+    pushd ${PREFIX}/${target_machine}-${ctng_vendor}-linux-gnu/sysroot/${dn}
+    for fdn in $(ls -1d *); do
+      ln -s ${PREFIX}/${target_machine}-${ctng_vendor}-linux-gnu/sysroot/${dn}${fdn} ${PREFIX}/${target_machine}-conda-linux-gnu/sysroot/${dn}${fdn}
+    done
+    popd
+  done
+  popd
 fi
