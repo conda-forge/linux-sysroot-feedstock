d8a663ce392a195af10e21b14ae561eba960bf35
Matthew R Becker <becker.mr@gmail.com>
2020-06-06 08:47:38 -0500

    BUG make symlinks in dirs

--- recipe/build-sysroot.sh
+++ recipe/build-sysroot.sh
@@ -24,5 +24,15 @@ ln -s $PWD/lib64 $PWD/lib
 popd
 
 if [[ ${ctng_vendor} == "conda_cos7" ]]; then
-  ln -s ${PREFIX}/${target_machine}-${ctng_vendor}-linux-gnu ${PREFIX}/${target_machine}-conda-linux-gnu
+  mkdir -p ${PREFIX}/${target_machine}-conda-linux-gnu/sysroot
+  pushd ${PREFIX}/${target_machine}-${ctng_vendor}-linux-gnu/sysroot
+  for dn in $(ls -d */); do
+    mkdir -p ${PREFIX}/${target_machine}-conda-linux-gnu/sysroot/${dn}
+    pushd ${PREFIX}/${target_machine}-${ctng_vendor}-linux-gnu/sysroot/${dn}
+    for fdn in $(ls -1d *); do
+      ln -s ${PREFIX}/${target_machine}-${ctng_vendor}-linux-gnu/sysroot/${dn}${fdn} ${PREFIX}/${target_machine}-conda-linux-gnu/sysroot/${dn}${fdn}
+    done
+    popd
+  done
+  popd
 fi
