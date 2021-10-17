bfa8991ec4f1934b00ae3e19ef5c80b8fc6f1acc
Ista Zahn <istazahn@gmail.com>
2021-10-17 16:45:34 -0400

    remove libnsl

--- recipe/build-sysroot.sh
+++ recipe/build-sysroot.sh
@@ -12,6 +12,20 @@ cp -Rf "${SRC_DIR}"/binary-glibc-common/* .
 
 mv usr/lib/* usr/lib64/
 rm -rf usr/lib
+
+## Linking or building against libsnsl produces binaries that don't run on recent Linux distributions.
+## Libraries and headers removed here to prevent this. See
+## https://github.com/conda-forge/rasterio-feedstock/issues/220
+rm lib64/libnsl*.so*
+rm usr/lib64/libnsl.{a,so}
+rm usr/include/rpcsvc/ypclnt.h
+rm usr/include/rpcsvc/yp.h
+rm usr/include/rpcsvc/yppasswd.h
+rm usr/include/rpcsvc/yppasswd.x
+rm usr/include/rpcsvc/yp_prot.h
+rm usr/include/rpcsvc/ypupd.h
+rm usr/include/rpcsvc/yp.x
+
 ln -s $PWD/lib64 $PWD/lib
 
 ln -s $PWD/usr/lib64 $PWD/usr/lib
