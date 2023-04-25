e1a0138455df4c6119a66cfd8be9bd9ccb4b9f68
beckermr <becker.mr@gmail.com>
2023-04-25 08:07:42 -0500

    add hashes for aarch64

--- recipe/gen_langpack_meta.py
+++ recipe/gen_langpack_meta.py
@@ -11,35 +11,38 @@ def _hash_it(url):
     return h.hexdigest()
 
 
-centos_machine = "x86_64"
-base_url = (
-    "https://repo.almalinux.org/almalinux/8/BaseOS/"
-    f"{centos_machine}/os/Packages"
-)
-r = requests.get(base_url)
-r.raise_for_status()
+for centos_machine in [
+    "x86_64",
+    "aarch64",
+    "ppc64le",
+    "s390x",
+]:
+    base_url = (
+        "https://repo.almalinux.org/almalinux/8/BaseOS/"
+        f"{centos_machine}/os/Packages"
+    )
+    r = requests.get(base_url)
+    r.raise_for_status()
 
-soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-langpacks = []
-for tag in soup.find_all("a"):
-    if tag.text.startswith("glibc-langpack-"):
-        langpacks.append(tag.text)
+    langpacks = []
+    for tag in soup.find_all("a"):
+        if tag.text.startswith("glibc-langpack-"):
+            langpacks.append(tag.text)
 
-langpacks = sorted(langpacks)
-jobs = [
-    joblib.delayed(_hash_it)(os.path.join(base_url, langpack))
-    for langpack in langpacks
-]
-with joblib.Parallel(n_jobs=8, backend="threading", verbose=100) as par:
-    shas = par(jobs)
+    langpacks = sorted(langpacks)
+    jobs = [
+        joblib.delayed(_hash_it)(os.path.join(base_url, langpack))
+        for langpack in langpacks
+    ]
+    with joblib.Parallel(n_jobs=8, backend="threading", verbose=0) as par:
+        shas = par(jobs)
 
-print("=" * 80)
-print("=" * 80)
-print("{% set langpacks_" + centos_machine + " = [")
-vals = []
-for lp, sh in zip(langpacks, shas):
-    nm = "-".join(lp.split("-")[0:3])
-    vals.append(f"    (\"{nm}\", \"{lp}\", \"{sh}\")")
-print(",\n".join(vals))
-print("] %}")
+    print("{% set langpacks_" + centos_machine + " = [")
+    vals = []
+    for lp, sh in zip(langpacks, shas):
+        nm = "-".join(lp.split("-")[0:3])
+        vals.append(f"    (\"{nm}\", \"{lp}\", \"{sh}\")")
+    print(",\n".join(vals))
+    print("] %}")
