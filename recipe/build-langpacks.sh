29a427b5114da5e79170727e545bbd95bebe9699
beckermr <becker.mr@gmail.com>
2023-04-25 20:32:28 -0500

    make usr directory duh

--- recipe/build-langpacks.sh
+++ recipe/build-langpacks.sh
@@ -3,6 +3,7 @@
 mkdir -p ${PREFIX}/${target_machine}-${ctng_vendor}-linux-gnu/sysroot
 pushd ${PREFIX}/${target_machine}-${ctng_vendor}-linux-gnu/sysroot > /dev/null 2>&1
 
+mkdir -p usr
 cp -Rf "${SRC_DIR}"/binary-glibc-langpacks/* usr/
 for dr in $(compgen -G "${SRC_DIR}"'/binary-glibc-langpack-*'); do
     if [[ "$(basename ${dr})" != "binary-glibc-langpack-en" ]]; then
