2600c438ab6070c44b3b030c2079b364b35c08be
beckermr <becker.mr@gmail.com>
2023-12-20 16:30:13 -0600

    REF only remove a few things

--- recipe/build-sysroot.sh
+++ recipe/build-sysroot.sh
@@ -27,14 +27,9 @@ rm usr/include/rpcsvc/ypupd.h
 rm usr/include/rpcsvc/yp.x
 
 # we supply libcrypt/libxcrypt as a separate package
-rm include/crypt.h
 rm usr/include/crypt.h
 rm lib64/libcrypt*.so*
 rm usr/lib64/libcrypt.{a,so}
-rm lib64/libxcrypt*.so*
-rm usr/lib64/libxcrypt.{a,so}
-rm usr/lib/pkgconfig/libcrypt.pc
-rm usr/lib/pkgconfig/libxcrypt.pc
 
 ln -s $PWD/lib64 $PWD/lib
 
