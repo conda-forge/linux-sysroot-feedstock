abb35ccbe396a978d023127e930401ecbc65685a
Isuru Fernando <isuruf@gmail.com>
2024-10-23 09:02:04 -0500

    update the update.py file

--- recipe/update.py
+++ recipe/update.py
@@ -10,6 +10,7 @@ import logging
 import requests
 import os
 from ruamel_yaml import BaseLoader, load
+from packaging.version import Version
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-log', '--loglevel', default='warning')
@@ -28,15 +29,50 @@ config = load(cbc_content, Loader=BaseLoader)
 rpm_arches = config["centos_machine"]
 conda_arches = config["cross_target_platform"]
 
+alma_version = "8.9"
+
 url_template = (
-    f"https://repo.almalinux.org/vault/{config['alma_version'][0]}"
+    f"https://repo.almalinux.org/vault/{alma_version}"
     # second part intententionally not filled yet
     "/{subfolder}/{arch}/os/Packages"
 )
 
-el_ver = "el" + config["alma_version"][0].replace(".", "_")
-glibc_string = f"{config['glibc_version'][0]}-{config['glibc_build1'][0]}.{el_ver}.{config['glibc_build2'][0]}"
-kernel_headers_string = f"{config['kernel_headers_version'][0]}-{config['kernel_headers_build'][0]}.{el_ver}"
+el_ver = "el" + alma_version.replace(".", "_")
+
+a = requests.get(url_template.format(subfolder="BaseOS", arch="x86_64")).content.decode('utf-8')
+
+glibc_build1 = 0
+glibc_build2 = 0
+glibc_version = 0
+kernel_headers_build = Version("0.0.0")
+kernel_headers_version = 0
+
+for line in a.splitlines():
+    if not line.startswith("<a href=\""):
+        continue
+    line = line[len("<a href=\""):]
+    url = line[:line.index("\">")]
+
+    if el_ver not in url:
+        continue
+
+    if not url.endswith("x86_64.rpm"):
+        continue
+
+    name, version, build = url.rsplit("-", 2)
+    # glibc-2.28-236.el8.7.x86_64.rpm
+    if name == "glibc":
+        glibc_build1 = max(glibc_build1, int(build.split(".")[0]))
+        glibc_build2 = max(glibc_build2, int(build.split(".")[2]))
+        glibc_version = version
+
+    # kernel-headers-4.18.0-513.24.1.el8_9.x86_64.rpm
+    if name == "kernel-headers":
+        kernel_headers_build = max(kernel_headers_build, Version(build.rsplit(".", 3)[0]))
+        kernel_headers_version = version
+
+glibc_string = f"{glibc_version}-{glibc_build1}.{el_ver}.{glibc_build2}"
+kernel_headers_string = f"{kernel_headers_version}-{kernel_headers_build}.{el_ver}"
 
 out_lines = []
 
@@ -53,6 +89,7 @@ name2string = {
     "kernel-headers": kernel_headers_string,
     "nss_db": glibc_string,
     # manual versions
+    # TODO: calculate them automatically as well
     "nss_nis": "3.0-8.el8",
     "nss-softokn-freebl": "3.90.0-6.el8_9",
 }
@@ -76,12 +113,8 @@ for pkg, string in name2string.items():
         "{{ powertools_rpm_url }}" if subfolder == "PowerTools" else
         "{{ appstream_rpm_url }}"
     )
-    string_jinja = (
-        "{{ glibc_string }}" if string == glibc_string else
-        "{{ kernel_headers_string }}" if string == kernel_headers_string else string
-    )
     # quadruple curly braces to keep {{ }} jinja templates
-    out_lines.append(f"    url: {url_jinja}/{pkg}-{string_jinja}.{{{{ centos_machine }}}}.rpm")
+    out_lines.append(f"    url: {url_jinja}/{pkg}-{string}.{{{{ centos_machine }}}}.rpm")
 
     for rpm_arch, conda_arch in zip(rpm_arches, conda_arches):
         rpm_url = (
@@ -97,4 +130,28 @@ for pkg, string in name2string.items():
         out_lines.append(f'    sha256: {sha}  # [cross_target_platform == "{conda_arch}"]')
     out_lines.append("")
 
-print("\n".join(out_lines))
+new_meta = []
+
+with open("meta.yaml") as f:
+    old_meta = f.readlines()
+
+skip = False
+for line in old_meta:
+    if line.startswith("{% set alma_version"):
+        line = "{% set alma_version = \"" + alma_version + "\" %}"
+    if line.startswith("{% set glibc_version"):
+        line = "{% set glibc_version = \"" + glibc_version + "\" %}"
+    if line.startswith("{% set kernel_headers_version"):
+        line = "{% set kernel_headers_version = \"" + kernel_headers_version + "\" %}"
+    if line.startswith("  # END source"):
+        skip = False
+        new_meta.extend(out_lines)
+    if not skip:
+        new_meta.append(line.rstrip())
+    if line.startswith("  # START source"):
+        skip = True
+
+new_meta.append("")
+
+with open("meta.yaml", "w") as f:
+    f.write("\n".join(new_meta))
